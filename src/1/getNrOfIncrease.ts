b3c7771664c9dc70e02b49ccbda83af602a98c9e
Robin <robinsilverhav@gmail.com>
2021-12-02 23:05:38 +0100

    using slice reduce instead of for-loop when calculating sum

--- src/1/getNrOfIncrease.ts
+++ src/1/getNrOfIncrease.ts
@@ -8,11 +8,8 @@ export function getNrOfIncrease({
   let totalIncrease = 0
 
   let prevSum = Number.MAX_SAFE_INTEGER
-  for (let index = 0; index <= depthValues.length + nrInSum; index++) {
-    let currSum = 0
-    for (let sumOffset = 0; sumOffset < nrInSum; sumOffset++) {
-      currSum += depthValues[index + sumOffset]
-    }
+  for (let index = 0; index <= depthValues.length - nrInSum; index++) {
+    const currSum = depthValues.slice(index, index + nrInSum).reduce((sum, i) => sum + i, 0)
     if (currSum > prevSum) {
       totalIncrease += 1
     }
