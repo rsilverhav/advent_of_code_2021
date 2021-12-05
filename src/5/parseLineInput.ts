caeb6d68392ec24d4c94a0f6b68b957f2b889b1a
Robin <robinsilverhav@gmail.com>
2021-12-05 10:18:28 +0100

    error checking in parseLineInputs

--- src/5/parseLineInput.ts
+++ src/5/parseLineInput.ts
@@ -10,6 +10,10 @@ export interface Line {
 export function parseLineInput(lineInputs: string[]): Line[] {
   const regex = /(\d+),(\d+) -> (\d+),(\d+)/
   return lineInputs.map((lineInput) => {
+    const regexRes = regex.exec(lineInput)
+    if ((regexRes?.length ?? 0) < 5) {
+      throw new Error(`Failed parsing line input with regex: ${lineInput}`)
+    }
     const [_, startX, startY, endX, endY] = regex.exec(lineInput) ?? []
     return {
       start: { x: Number(startX), y: Number(startY) },
